dd16166016086f7bd0a74ab63138575c7e5cad2b
denyschr <denprogman99@gmail.com>
2024-08-06 16:25:41 +0300

    feat: add ngrx store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    provideStore()
   ]
 };
