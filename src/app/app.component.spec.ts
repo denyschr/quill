fe94b53b8e633ec3507c4860906d25a9ec971643
denyschr <denprogman99@gmail.com>
2024-08-31 14:50:15 +0300

    test: dispatch `getCurrentUser` action

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,43 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { provideRouter } from '@angular/router';
-import { provideMockStore } from '@ngrx/store/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { By } from '@angular/platform-browser';
 import { ValidationDefaultsComponent } from '@shared/ui/validation-defaults';
+import { authActions } from '@auth/data-access/store';
 
 describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
+  let store: MockStore;
+  const initialState = {
+    auth: {
+      currentUser: undefined,
+      submitting: false,
+      loading: false,
+      errors: null
+    }
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [AppComponent],
-      providers: [provideRouter([]), provideMockStore({})]
+      providers: [provideRouter([]), provideMockStore({ initialState })]
     }).compileComponents();
 
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
+    spyOn(store, 'dispatch').and.callThrough();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should dispatch getCurrentUser', () => {
+    expect(store.dispatch).toHaveBeenCalledWith(authActions.getCurrentUser());
   });
 
   it('should have a router outlet', () => {
