edcf5c4ee45b2459dd3580f799849acfdb08784c
denyschr <denprogman99@gmail.com>
2024-10-23 21:35:27 +0300

    feat(navbar): improve styling

--- src/app/layout/ui/navbar/navbar.component.ts
+++ src/app/layout/ui/navbar/navbar.component.ts
@@ -10,90 +10,88 @@ import { combineLatest } from 'rxjs';
   selector: 'ql-navbar',
   standalone: true,
   template: `
-    <header>
-      <nav class="navbar navbar-expand-lg shadow-sm bg-white">
-        <div class="container">
-          <a class="navbar-brand fw-bold text-success" routerLink="/">Quill</a>
-          <button type="button" class="navbar-toggler" (click)="toggleNavbar()">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div
-            id="navbar"
-            class="navbar-collapse"
-            [ngbCollapse]="navbarCollapsed"
-            *ngrxLet="vm$; let vm"
-          >
-            @let currentUser = vm.currentUser;
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container">
+        <a class="navbar-brand" routerLink="/">Quill</a>
+        <button type="button" class="navbar-toggler" (click)="toggleNavbar()">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div
+          id="navbar"
+          class="navbar-collapse"
+          [ngbCollapse]="navbarCollapsed"
+          *ngrxLet="vm$; let vm"
+        >
+          @let currentUser = vm.currentUser;
 
-            <!-- Show this if the user is not logged in -->
-            @if (currentUser === null) {
-              <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                  <a
-                    class="nav-link"
-                    routerLink="/"
-                    routerLinkActive="active"
-                    [routerLinkActiveOptions]="{ exact: true }"
-                    >Home</a
-                  >
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" routerLink="/login" routerLinkActive="active">Sign in</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" routerLink="/register" routerLinkActive="active">Sign up</a>
-                </li>
-              </ul>
-            }
+          <!-- Show this if the user is not logged in -->
+          @if (currentUser === null) {
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item">
+                <a
+                  class="nav-link"
+                  routerLink="/"
+                  routerLinkActive="active"
+                  [routerLinkActiveOptions]="{ exact: true }"
+                  >Home</a
+                >
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" routerLink="/login" routerLinkActive="active">Sign in</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" routerLink="/register" routerLinkActive="active">Sign up</a>
+              </li>
+            </ul>
+          }
 
-            <!-- Show this if the user is logged in -->
-            @if (currentUser) {
-              <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                  <a
-                    class="nav-link"
-                    routerLink="/"
-                    routerLinkActive="active"
-                    [routerLinkActiveOptions]="{ exact: true }"
-                    >Home</a
-                  >
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" routerLink="/editor" routerLinkActive="active">
-                    <i class="bi bi-pencil-square"></i>
-                    New Article
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" routerLink="/settings" routerLinkActive="active">
-                    <i class="bi bi-gear-wide-connected"></i>
-                    Settings
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a
-                    class="nav-link"
-                    [routerLink]="['/profile', currentUser.username]"
-                    routerLinkActive="active"
-                  >
-                    @if (currentUser.image) {
-                      <img
-                        class="rounded-circle me-1"
-                        width="26"
-                        height="26"
-                        [src]="currentUser.image"
-                        [alt]="currentUser.username"
-                      />
-                    }
-                    {{ currentUser.username }}
-                  </a>
-                </li>
-              </ul>
-            }
-          </div>
+          <!-- Show this if the user is logged in -->
+          @if (currentUser) {
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item">
+                <a
+                  class="nav-link"
+                  routerLink="/"
+                  routerLinkActive="active"
+                  [routerLinkActiveOptions]="{ exact: true }"
+                  >Home</a
+                >
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" routerLink="/editor" routerLinkActive="active">
+                  <i class="bi bi-pencil-square"></i>
+                  New Article
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" routerLink="/settings" routerLinkActive="active">
+                  <i class="bi bi-gear-wide-connected"></i>
+                  Settings
+                </a>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link"
+                  [routerLink]="['/profile', currentUser.username]"
+                  routerLinkActive="active"
+                >
+                  @if (currentUser.image) {
+                    <img
+                      class="rounded-circle me-1"
+                      width="26"
+                      height="26"
+                      [src]="currentUser.image"
+                      [alt]="currentUser.username"
+                    />
+                  }
+                  {{ currentUser.username }}
+                </a>
+              </li>
+            </ul>
+          }
         </div>
-      </nav>
-    </header>
+      </div>
+    </nav>
   `,
   imports: [RouterLink, RouterLinkActive, LetDirective, NgbCollapse],
   changeDetection: ChangeDetectionStrategy.OnPush
