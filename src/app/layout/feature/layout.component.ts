804aea2959c81f4c3e48081c7851ad9fcae900f9
denyschr <denprogman99@gmail.com>
2024-10-23 21:58:06 +0300

    feat(layout): remove footer

--- src/app/layout/feature/layout.component.ts
+++ src/app/layout/feature/layout.component.ts
@@ -1,17 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '@layout/ui/navbar';
-import { FooterComponent } from '@layout/ui/footer';
 
 @Component({
   selector: 'ql-layout',
   standalone: true,
   template: `
     <ql-navbar />
-    <main class="flex-grow-1">
+    <main>
       <router-outlet />
     </main>
-    <ql-footer />
   `,
   styles: [
     `
@@ -23,7 +21,7 @@ import { FooterComponent } from '@layout/ui/footer';
       }
     `
   ],
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class LayoutComponent {}
