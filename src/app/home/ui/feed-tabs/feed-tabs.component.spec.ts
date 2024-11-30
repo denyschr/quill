2fc630373b48c704edf07d3c3b2a59e3568d8b86
denyschr <denprogman99@gmail.com>
2024-11-30 18:07:09 +0200

    test(feed-tabs): add unit tests

--- src/app/home/ui/feed-tabs/feed-tabs.component.spec.ts
+++ src/app/home/ui/feed-tabs/feed-tabs.component.spec.ts
@@ -1,76 +1,133 @@
-/* eslint-disable @angular-eslint/prefer-on-push-component-change-detection */
-import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FeedToggleComponent } from './feed-tabs.component';
+import { FeedTabsComponent } from './feed-tabs.component';
+import { By } from '@angular/platform-browser';
 
-@Component({
-  standalone: true,
-  template: `<ql-feed-toggle
-    [feedType]="feedType"
-    [feedDisabled]="feedDisabled"
-    [selectedTag]="selectedTag"
-    (selectFeed)="selectedFeed = true"
-  />`,
-  imports: [FeedToggleComponent]
-})
-class FeedToggleTestComponent {
-  public feedType = 'all';
-  public feedDisabled = true;
-  public selectedTag?: string;
-  public selectedFeed = false;
-}
-
-describe('FeedToggleComponent', () => {
-  let fixture: ComponentFixture<FeedToggleTestComponent>;
+describe('FeedTabsComponent', () => {
+  let component: FeedTabsComponent;
+  let fixture: ComponentFixture<FeedTabsComponent>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [FeedTabsComponent]
+    });
 
-    fixture = TestBed.createComponent(FeedToggleTestComponent);
+    fixture = TestBed.createComponent(FeedTabsComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should display the global feed by default', () => {
-    const element = fixture.nativeElement as HTMLElement;
-    const globalFeed = element.querySelector<HTMLAnchorElement>('#global-feed')!;
-    expect(globalFeed).not.toBeNull();
-    expect(globalFeed.textContent).toContain('Global Feed');
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 
-  it('should emit an event when the global feed is selected', () => {
-    const element = fixture.nativeElement as HTMLElement;
-    const globalFeed = element.querySelector<HTMLAnchorElement>('#global-feed')!;
-    globalFeed.click();
-    expect(fixture.componentInstance.selectedFeed).toBeTruthy();
+  it('should show one tab by default', () => {
+    const element = fixture.debugElement;
+    const tabLinks = element.queryAll(By.css('.nav.nav-tabs .nav-item a.nav-link'));
+    expect(tabLinks.length).withContext('You should only have one tab for a global feed').toBe(1);
+    expect(tabLinks[0].nativeElement.textContent).toContain('Global Feed');
   });
 
-  it('should display the user`s feed if logged in', () => {
-    fixture.componentInstance.feedDisabled = false;
+  it('should show two tabs if logged in', () => {
+    fixture.componentRef.setInput('feedDisabled', false);
     fixture.detectChanges();
 
-    const element = fixture.nativeElement as HTMLElement;
-    const yourFeed = element.querySelector<HTMLAnchorElement>('#your-feed')!;
-    expect(yourFeed).not.toBeNull();
-    expect(yourFeed.textContent).toContain('Your Feed');
+    const element = fixture.debugElement;
+    const tabLinks = element.queryAll(By.css('.nav.nav-tabs .nav-item a.nav-link'));
+    expect(tabLinks.length)
+      .withContext("You should have two tabs: one for a global feed and one for the user's feed")
+      .toBe(2);
+    expect(tabLinks[0].nativeElement.textContent).toContain('Global Feed');
+    expect(tabLinks[1].nativeElement.textContent).toContain('Your Feed');
   });
 
-  it('should emit an event when the user`s feed is selected', () => {
-    fixture.componentInstance.feedDisabled = false;
+  it('should show three tabs if logged in and a tag is selected', () => {
+    fixture.componentRef.setInput('feedDisabled', false);
+    fixture.componentRef.setInput('tag', 'ipsum');
     fixture.detectChanges();
 
-    const element = fixture.nativeElement as HTMLElement;
-    const yourFeed = element.querySelector<HTMLAnchorElement>('#your-feed')!;
-    yourFeed.click();
-    expect(fixture.componentInstance.selectedFeed).toBeTruthy();
+    const element = fixture.debugElement;
+    const tabLinks = element.queryAll(By.css('.nav.nav-tabs .nav-item a.nav-link'));
+    expect(tabLinks.length)
+      .withContext(
+        "You should have three tabs: one for a global feed, one for the user's feed and one for the feed with the selected tag"
+      )
+      .toBe(3);
+    expect(tabLinks[0].nativeElement.textContent).toContain('Global Feed');
+    expect(tabLinks[1].nativeElement.textContent).toContain('Your Feed');
+    expect(tabLinks[2].nativeElement.textContent).toContain('#ipsum');
   });
 
-  it('should display selected tag if provided', () => {
-    fixture.componentInstance.selectedTag = 'ipsum';
+  it('should make the first tab active when showing a global feed', () => {
+    fixture.componentRef.setInput('feedDisabled', false);
     fixture.detectChanges();
 
-    const element = fixture.nativeElement as HTMLElement;
-    const tag = element.querySelector<HTMLAnchorElement>('#tag')!;
-    expect(tag).not.toBeNull();
-    expect(tag.textContent).toContain('ipsum');
+    const element = fixture.debugElement;
+    const links = element.queryAll(By.css('a.nav-link'));
+    expect(links.length).withContext('You should have two links').toBe(2);
+    expect(links[0].nativeElement.className)
+      .withContext('The first link should be active')
+      .toContain('active');
+    expect(links[1].nativeElement.className)
+      .withContext('The second link should not be active')
+      .not.toContain('active');
+  });
+
+  it("should make the second tab active when showing the user's feed", () => {
+    fixture.componentRef.setInput('feedType', 'feed');
+    fixture.componentRef.setInput('feedDisabled', false);
+    fixture.detectChanges();
+
+    const element = fixture.debugElement;
+    const links = element.queryAll(By.css('a.nav-link'));
+    expect(links.length).withContext('You should have two links').toBe(2);
+    expect(links[0].nativeElement.className)
+      .withContext('The first link should not be active')
+      .not.toContain('active');
+    expect(links[1].nativeElement.className)
+      .withContext('The second link should be active')
+      .toContain('active');
+  });
+
+  it('should make the third tab active when showing a feed with the selected tag', () => {
+    fixture.componentRef.setInput('feedType', 'feed');
+    fixture.componentRef.setInput('feedDisabled', false);
+    fixture.componentRef.setInput('tag', 'ipsum');
+    fixture.detectChanges();
+
+    const element = fixture.debugElement;
+    const links = element.queryAll(By.css('a.nav-link'));
+    expect(links.length).withContext('You should have three links').toBe(3);
+    expect(links[0].nativeElement.className)
+      .withContext('The first link should not be active')
+      .not.toContain('active');
+    expect(links[1].nativeElement.className)
+      .withContext('The second link should not be active')
+      .not.toContain('active');
+    expect(links[2].nativeElement.className)
+      .withContext('The third link should be active')
+      .toContain('active');
+  });
+
+  it('should emit an event on feed change', () => {
+    spyOn(component.changed, 'emit');
+    fixture.componentRef.setInput('feedDisabled', false);
+    fixture.detectChanges();
+
+    const element = fixture.debugElement;
+    const links = element.queryAll(By.css('a.nav-link'));
+    expect(links.length).withContext('You should have two links').toBe(2);
+
+    links[1].nativeElement.click();
+    expect(component.changed.emit)
+      .withContext('You should emit an event on change')
+      .toHaveBeenCalledWith('feed');
+
+    fixture.componentRef.setInput('feedType', 'feed');
+    fixture.detectChanges();
+
+    links[0].nativeElement.click();
+    expect(component.changed.emit)
+      .withContext('You should emit an event on change')
+      .toHaveBeenCalledWith('global');
   });
 });
