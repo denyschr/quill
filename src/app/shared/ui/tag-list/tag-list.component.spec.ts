abb1854ab132cd2260b97931ea090095f24062bf
denyschr <denprogman99@gmail.com>
2025-01-16 15:16:36 +0200

    test(tag-list): refactor unit tests

--- src/app/shared/ui/tag-list/tag-list.component.spec.ts
+++ src/app/shared/ui/tag-list/tag-list.component.spec.ts
@@ -1,30 +1,20 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { TagListComponent } from './tag-list.component';
 
 describe('TagListComponent', () => {
-  let component: TagListComponent;
-  let fixture: ComponentFixture<TagListComponent>;
-
-  const tags = ['tag one', 'tag two'];
-
   beforeEach(() => {
     TestBed.configureTestingModule({});
-
-    fixture = TestBed.createComponent(TagListComponent);
-    component = fixture.componentInstance;
-    fixture.componentRef.setInput('tags', tags);
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
   });
 
   it('should display a list of tags', () => {
+    const fixture = TestBed.createComponent(TagListComponent);
     const element: HTMLElement = fixture.nativeElement;
+    fixture.componentRef.setInput('tags', ['dragons', 'training']);
+    fixture.detectChanges();
+
     const tagNames = element.querySelectorAll('li');
-    expect(tagNames.length).withContext('You need three `li` elements for tag names').toBe(2);
-    expect(tagNames[0].textContent).toContain('tag one');
-    expect(tagNames[1].textContent).toContain('tag two');
+    expect(tagNames.length).withContext('You should have a `li` element for each tag name').toBe(2);
+    expect(tagNames[0].textContent).toContain('dragons');
+    expect(tagNames[1].textContent).toContain('training');
   });
 });
